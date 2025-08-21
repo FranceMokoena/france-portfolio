8df47950d6f1d17ebf4495a4b5d9648e5ee6affe
FranceMokoena <developer091120@gmail.com>
2025-08-21 08:05:29 -0700

    Fix server configuration for Render deployment

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
 const port = process.env.PORT || 10000;
 
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -24,7 +23,7 @@ app.prepare().then(() => {
       console.error(err);
       process.exit(1);
     })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+    .listen(port, '0.0.0.0', () => {
+      console.log(`> Ready on http://0.0.0.0:${port}`);
     });
 });
